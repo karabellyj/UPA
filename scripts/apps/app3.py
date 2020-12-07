35a48ce0a012069b2f4ebed6fdba949419a62cdc
Jozef Karabelly <jozefkarabelly@gmail.com>
2020-12-07 11:38:19 +0100

    finish query B

--- scripts/apps/app3.py
+++ scripts/apps/app3.py
@@ -29,8 +29,21 @@ def find_cointegrated_pairs(data):
             score_matrix[i, j] = score
             pvalue_matrix[i, j] = pvalue
             if pvalue < 0.05:
-                pairs.append((keys[i], keys[j]))
+                pairs.append((keys[i], keys[j], pvalue))
     return score_matrix, pvalue_matrix, pairs
+
+def generate_table(pairs):
+    table_header = [
+        html.Thead(html.Tr([html.Th("Pairs with p-value < 0.05"), html.Th("p-value")]))
+    ]
+    rows = []
+    for pair in pairs:
+        rows.append(html.Tr([html.Td(f'{pair[0]}/{pair[1]}'), html.Td(pair[2])]))
+
+    table_body = [html.Tbody(rows)]
+
+    table = dbc.Table(table_header + table_body, bordered=True)
+    return table
 
 storage = PostgresStorage()
 
@@ -49,6 +62,7 @@ layout = html.Div([
         id=f'heatmap',
         figure=px.imshow(pvalues, x=df.columns, y=df.columns)
     ), width={'size': 6, 'offset': 3})),
+    dbc.Row(dbc.Col(generate_table(sorted(pairs, key=lambda x: x[2])), width={'size': 4, 'offset': 4})),
     dcc.Link('Go to App 1', href='/apps/app1'),
     dcc.Link('Go to App 2', href='/apps/app2')
 ], style={})
